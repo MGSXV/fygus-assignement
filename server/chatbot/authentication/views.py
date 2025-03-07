402be9da3516813f0f48087d52cf9b4a1ba8007b
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-03-08 00:41:04 +0100

    Remove token blacklisting in LogoutView to prevent unintended side effects

--- server/chatbot/authentication/views.py
+++ server/chatbot/authentication/views.py
@@ -29,7 +29,7 @@ class LogoutView(APIView):
         try:
             refresh_token = request.data["refresh"]
             token = RefreshToken(refresh_token)
-            token.blacklist()
+            # token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
