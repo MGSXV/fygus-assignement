394a6f4b3fab04b1adebb1a6de1fb94984922275
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-03-07 23:18:41 +0100

    Implement custom login view for user authentication

--- server/chatbot/authentication/urls.py
+++ server/chatbot/authentication/urls.py
@@ -1,10 +1,9 @@
-# authentication/urls.py
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SignupView, LogoutView
+from .views import SignupView, LogoutView, CustomLoginView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('login/', CustomLoginView.as_view(), name='custom_login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
--- server/chatbot/authentication/views.py
+++ server/chatbot/authentication/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
 
 class SignupView(APIView):
     permission_classes = [AllowAny]
@@ -32,3 +33,32 @@ class LogoutView(APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class CustomLoginView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            
+            return Response({
+                'refresh': str(refresh),
+                'access': access_token,
+                'user': {
+                    'id': user.id,
+                    'username': user.username,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                }
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                'error': 'Invalid credentials'
+            }, status=status.HTTP_401_UNAUTHORIZED)
